227af59a21b17ff3a9e68e51a014236f8226dcd1
lostmind84 <dakeyras.crypto@gmail.com>
2020-10-15 17:33:22 +0200

    Integrate web3modal

--- packages/front/src/MainLayout.js
+++ packages/front/src/MainLayout.js
@@ -1,12 +1,15 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import 'antd/dist/antd.css';
 import './mainLayout.css';
-import { Layout, Menu, Breadcrumb } from 'antd';
+import { Layout, Menu, Breadcrumb, Button } from 'antd';
+import { Row, Col } from 'antd';
 import {
     DesktopOutlined,
     PieChartOutlined
 } from '@ant-design/icons';
 import { Link } from "react-router-dom";
+import Web3 from "web3";
+import Web3Modal from "web3modal";
 
 const { Header, Content, Footer, Sider } = Layout;
 const { SubMenu } = Menu;
@@ -16,6 +19,27 @@ function MainLayout(props) {
 
     const onCollapse = collapsed => {
         setCollapsed(collapsed);
+    };
+
+    const loadWeb3Provider = async () => {
+        console.log('Connecting to provider.....');
+        const providerOptions = {
+        };
+        const web3Modal = new Web3Modal({
+            network: 'mainnet', // optional
+            // network: 'ropsten', // optional
+            cacheProvider: true, // optional
+            providerOptions
+        });
+        const provider = await web3Modal.connect();
+        // If you want to use web3, uncomment this: web3 = new Web3(provider);
+        const provider_ = new Web3(provider);
+        console.log(provider);
+        const signer = provider_.getSigner();
+        const balance = await provider_.getBalance(await signer.getAddress());
+        console.log('.... connected');
+        console.log(balance);
+        //console.log(provider_);
     };
 
     return (
@@ -41,7 +65,14 @@ function MainLayout(props) {
                     </Menu>
                 </Sider>
                 <Layout className="site-layout">
-                    <Header className="site-layout-background" style={{ padding: 0 }} />
+                    <Header className="site-layout-background" style={{ padding: 0 }}>
+                       <Row>
+                            <Col span={6} offset={20}>
+                                <Button type="primary" onClick={loadWeb3Provider} >Connect wallet</Button>
+                            </Col>
+                       </Row>
+                        
+                    </Header>
                     <Content style={{ margin: '0 16px' }}>
                         <Breadcrumb style={{ margin: '16px 0' }}>
                             <Breadcrumb.Item>Home</Breadcrumb.Item>
