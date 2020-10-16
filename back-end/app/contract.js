39135e39547cce271795a492e1dac3d4881a686b
vanhassel-vincent <vincent.vanhassel@gmail.com>
2020-10-16 16:01:24 +0200

    first implementation of the contract call

---
+++ back-end/app/contract.js
@@ -0,0 +1,13 @@
+const { ethers } = require("ethers");
+
+const provider = new ethers.providers.JsonRpcProvider();
+const signer = provider.getSigner()
+
+const badgeMinterAddress = "eththemall.eth";
+
+const badgeMinterAbi = [
+
+]
+
+const badgeMinterContract = new ethers.Contract(badgeMinterAddress, badgeMinterAbi, provider);
+const badgeMinterWithSigner = contract.connect(signer);
