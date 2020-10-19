db43651726631725cccd4bd6c8daf9c344f78553
Nemanja Cerovac <nemanja.cerovac@gmail.com>
2020-10-19 14:40:36 +0200

    48h left, no time for fancy skill tree - GO GRID MODE!

--- packages/front/src/Dashboard/Dashboard.js
+++ packages/front/src/Dashboard/Dashboard.js
@@ -1,61 +1,80 @@
 import React from 'react';
 import './Dashboard.css';
 
-import {
-  SkillTreeGroup,
-  SkillTree,
-  SkillProvider,
-  SkillType,
-  SkillGroupDataType,
-} from 'beautiful-skill-tree';
+import { Tabs, Row, Col, Divider } from 'antd';
+import { AppleOutlined, AndroidOutlined } from '@ant-design/icons';
 
-const data = [
-  {
-    id: 'hello-world',
-    title: 'Hello World',
-    tooltip: {
-      content:
-        'This node is the top most level, and will be unlocked, and ready to be clicked.',
-    },
-    children: [
-      {
-        id: 'hello-sun',
-        title: 'Hello Sun',
-        tooltip: {
-          content:
-            'This is a parent of the top node, and will locked while the parent isn’t in a selected state.',
-        },
-        children: [],
-      },
-      {
-        id: 'hello-stars',
-        title: 'Hello Stars',
-        tooltip: {
-          content:
-            'This is the child of ‘Hello World and the sibling of ‘Hello Sun’. Notice how the app takes care of the layout automatically? That’s why this is called Beautiful Skill Tree and not just ‘Skill Tree’. (Also the npm namespace had already been taken for the latter so (flick hair emoji).',
-        },
-        children: [],
-      },
-    ],
-  },
-];
+const { TabPane } = Tabs;
+
+const style = { background: '#0092ff', padding: '8px 0' };
 
 function Dashboard(props) {
   return (
     <div>
-      <SkillProvider>
-        <SkillTreeGroup>
-            {({ skillCount }: SkillGroupDataType) => (
-              <SkillTree
-                treeId="first-tree"
-                title="Skill Tree"
-                data={data}
-                collapsible
-                description="My first skill tree"
-              />
-            )}
-        </SkillTreeGroup>
-    </SkillProvider>
+      <Tabs defaultActiveKey="1">
+        <TabPane
+          tab={
+            <span>
+              <AppleOutlined />
+              Compound
+            </span>
+          }
+          key="1"
+        >
+          <Divider orientation="left">Borrow</Divider>
+          <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
+            <Col className="gutter-row" span={6}>
+              <div style={style}>col-6</div>
+            </Col>
+            <Col className="gutter-row" span={6}>
+              <div style={style}>col-6</div>
+            </Col>
+            <Col className="gutter-row" span={6}>
+              <div style={style}>col-6</div>
+            </Col>
+            <Col className="gutter-row" span={6}>
+              <div style={style}>col-6</div>
+            </Col>
+          </Row>
+          <Divider orientation="left">Supply</Divider>
+          <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
+            <Col className="gutter-row" span={6}>
+              <div style={style}>col-6</div>
+            </Col>
+            <Col className="gutter-row" span={6}>
+              <div style={style}>col-6</div>
+            </Col>
+            <Col className="gutter-row" span={6}>
+              <div style={style}>col-6</div>
+            </Col>
+            <Col className="gutter-row" span={6}>
+              <div style={style}>col-6</div>
+            </Col>
+          </Row>
+        </TabPane>
+        <TabPane
+          tab={
+            <span>
+              <AndroidOutlined />
+              AAVE
+            </span>
+          }
+          key="2"
+        >
+          Tab 2
+        </TabPane>
+        <TabPane
+          tab={
+            <span>
+              <AndroidOutlined />
+              UniSwap
+            </span>
+          }
+          key="3"
+        >
+          Tab 3
+        </TabPane>
+      </Tabs>
     </div>
   );
 }
