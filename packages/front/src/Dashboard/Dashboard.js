368f28ff38bf3d6e8ba2aace4b77a5441dd45f7f
Fabien F <lostmind84@users.noreply.github.com>
2020-10-20 22:59:35 +0200

    badges now use ipfs images fix bug when calling mint which was not working as intended

--- packages/front/src/Dashboard/Dashboard.js
+++ packages/front/src/Dashboard/Dashboard.js
@@ -7,8 +7,7 @@ import { GifOutlined, SketchOutlined } from '@ant-design/icons';
 const axios = require('axios').default;
 const instance = axios.create({
   baseURL: 'http://localhost:3001/',
-  timeout: 1000,
-  headers: { 'X-Custom-Header': 'foobar' }
+  timeout: 30000
 });
 
 const { TabPane } = Tabs;
@@ -25,74 +24,82 @@ const openNotification = (title, text) => {
   });
 };
 
-const clickMint = (nft) => {
-  console.log(`Minting ${nft.Action} !`);
-
-  if (ethAddr.indexOf('0x00') !== 0) {
-    instance.post(nft.Action, {
-      userAddress: ethAddr
-    })
-      .then(function (response) {
-        //console.log(`${nft.Action}: ${response.data}`);
-        if (response.data === true)
-          openNotification('Success', 'Badge Minted !');
-        else
-          openNotification('Sorry bro', 'You are not eligibile for this badge');
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  }
-  else {
-    openNotification('Error', 'Please connect your wallet first');
-  }
-};
-
-const projectList = (conf) => {
-  return (
-    conf.NFTS_PER_PROJECT.map((project) => {
-      return (
-        <TabPane
-          tab={
-            <span>
-              <SketchOutlined />
-              {project.ProjectName}
-            </span>
-          }
-          key={project.ProjectName}
-        >
-          <Row gutter={[16, { xs: 8, sm: 16, md: 24, lg: 32 }]}>
-            {project.NFTS.map((nft) => {
-              //console.log(nft.Minted);
-              return (
-                <Col className="gutter-row" span={6} key={nft.Action}>
-                  <Card title={nft.Title} hoverable
-                    extra={<Image src={nft.Icon}></Image>}
-                    actions={[
-                      <>
-                        {
-                          nft.Minted === "false"
-                            ? <Button type="primary" onClick={() => clickMint(nft)}>Mint</Button>
-                            : <Button type="ghost">Already Minted !</Button>
-                        }
-                      </>
-                    ]}>
-                    <Image src={nft.Minted === "false" ? nft.DefaultBadgeImg : nft.IpfsBadgeUri} />
-                  </Card>
-                </Col>)
-            })}
-          </Row>
-        </TabPane>
-      );
-    })
-  )
-};
-
 function Dashboard(props) {
   ethAddr = props.ethAddr;
   const [config, setConfig] = useState(defaultConfig);
+  const [badgeMinted, setBadgeMinted] = useState(false);
+
+  const projectList = (conf) => {
+    return (
+      conf.NFTS_PER_PROJECT.map((project) => {
+        return (
+          <TabPane
+            tab={
+              <span>
+                <SketchOutlined />
+                {project.ProjectName}
+              </span>
+            }
+            key={project.ProjectName}
+          >
+            <Row gutter={[16, { xs: 8, sm: 16, md: 24, lg: 32 }]}>
+              {project.NFTS.map((nft) => {
+                //console.log(nft.Minted);
+                return (
+                  <Col className="gutter-row" span={6} key={nft.Action}>
+                    <Card title={nft.Title} hoverable
+                      extra={<Image src={nft.Icon}></Image>}
+                      actions={[
+                        <>
+                          {
+                            nft.Minted === "false"
+                              ? <Button type="primary" onClick={() => clickMint(nft)}>Mint</Button>
+                              : <Button type="ghost">Already Minted !</Button>
+                          }
+                        </>
+                      ]}>
+                      {
+                        nft.Minted === "false"
+                          ? <Image src={`https://ipfs.io/ipfs/${nft.IpfsLockedBadgeHash}`} />
+                          : <Image src={nft.IpfsBadgeUri} />
+                      }
+                    </Card>
+                  </Col>)
+              })}
+            </Row>
+          </TabPane>
+        );
+      })
+    )
+  };
+
+  const clickMint = (nft) => {
+    //console.log(`Minting ${nft.Action} !`);
+  
+    if (ethAddr.indexOf('0x00') !== 0) {
+      instance.post(nft.Action, {
+        userAddress: ethAddr.toLocaleLowerCase()
+      })
+        .then(function (response) {
+          //console.log(`${nft.Action}: ${response.data}`);
+          if (response.data != null && response.data.isEligible === true) {
+            openNotification('Success', 'Badge Minted !');
+            setBadgeMinted(true);
+          }
+          else
+            openNotification('Sorry bro', 'You are not eligibile for this badge');
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
+    else {
+      openNotification('Error', 'Please connect your wallet first');
+    }
+  };
 
   useEffect(() => {
+    setBadgeMinted(false);
     // retrieve user NFTs
     if (ethAddr.indexOf('0x00') !== 0) {
       instance.post('/retrieveUserNTNFTBadges', {
@@ -113,13 +120,13 @@ function Dashboard(props) {
                   if (n.TemplateId == nft.templateId) {
                     //console.log('Found already minted template ! : ' + n.TemplateId)
                     n.Minted = true;
-                    n.IpfsBadgeUri = nft.badgeURI;
+                    n.IpfsBadgeUri = `https://ipfs.io/ipfs/${nft.templateData[2]}`;
                   }
                 });
               });
             });
 
-            setConfig({...configCopy});
+            setConfig({ ...configCopy });
           }
         })
         .catch(function (error) {
@@ -129,7 +136,7 @@ function Dashboard(props) {
     else {
       //openNotification('Error', 'Please connect your wallet first');
     }
-  }, [props.ethAddr]);
+  }, [props.ethAddr, badgeMinted]);
 
   return (
     <div>
